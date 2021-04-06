bcc559188da3157040f72c11633368090bc56da7
NathanWith <>
2021-04-06 23:10:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import os
+
+from keep_alive import keep_alive
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+      if message.content == "yes":
+        print("b is greater than a")
+
+
+
+
+
+
+
+
+keep_alive()
+
+token = os.environ.get("DISCORD_BOT_SECRET")
+client.run(token)
